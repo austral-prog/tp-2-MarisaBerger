8abfa5e412bdcf285593aa1a4b13b838c7061e80
MarisaBerger <163553459+MarisaBerger@users.noreply.github.com>
2024-03-22 12:35:16 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full_name = "AdA" + "LoVeLAce"
+    print(full_name.lower())
+    print(full_name.tittle)
+    print(full_name.upper())
+    
+    
