7846fab1a42dec3cecda08e8516089db7376cd50
MarisaBerger <163553459+MarisaBerger@users.noreply.github.com>
2024-03-22 17:52:28 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,9 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
-    full_name = "AdA" + "LoVeLAce"
+    full_name = f"{first_name} {last_name}"
     print(full_name.lower())
-    print(first_name.title() + last_name.title())
+    print(first_name.title(), last_name.title())
     print(full_name.upper())
     print("\t"+ full_name.lower())
     
