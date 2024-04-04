84760903166f6209490c804fce7464e184456057
MarisaBerger <163553459+MarisaBerger@users.noreply.github.com>
2024-04-04 18:36:02 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,15 @@
 def change():
     expense = 23.75
     money = 100
+    print("ingresar gasto")
+    print(expense)
+    print("dinero recibido")
+    print(money)
+    print()
+    print("vuelto")
+    print()
+
+    print("pesos:")
+    print(int(money - expense))
+    print("centavos:")
+    print(int(((money-expense)-int(money-expense))*100))
